5d41bf97719246f516a17c61d44cd6fd77dbfaf0
whwogns <whwogns1122@naver.com>
2017-04-24 14:07:57 +0900

    연습파일 삭제

--- ClassA.java
+++
@@ -1,8 +0,0 @@
-public class ClassA{
-  public static void main(String[] args) {
-
-  ggggggggg
-afgadfga
-
-  }
-}
--- ClassB.java
+++
@@ -1,5 +0,0 @@
-public class ClassB{
-  public static void main(String[] args) {
-      system.out.println("user2 작성");
-  }
-}
