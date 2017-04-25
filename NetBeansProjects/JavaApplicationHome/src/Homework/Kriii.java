c30e65b4d41d49c838c07fd04d92cc5343258e00
whwogns <whwogns1122@naver.com>
2017-04-25 23:17:52 +0900

    숙제업로드

---
+++ NetBeansProjects/JavaApplicationHome/src/Homework/Kriii.java
@@ -0,0 +1,9 @@
+
+package Homework;
+
+public class Kriii {
+    public static void main(String[] args){
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
---
+++ NetBeansProjects/JavaApplicationHome/src/Homework/Registration.java
@@ -0,0 +1,10 @@
+
+package Homework;
+
+public class Registration {
+    public static void main(String[] args){
+       
+        System.out.println(5);
+        System.out.println("whwogns12");
+    }
+  }
