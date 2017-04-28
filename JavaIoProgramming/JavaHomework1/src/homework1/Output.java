efc20455c5b6dc28a45d823a9f824e037874fa3b
whwogns <whwogns1122@naver.com>
2017-04-28 16:43:30 +0900

    웹 업데이트

--- JavaIoProgramming/JavaHomework1/src/homework1/Output.java
+++ JavaIoProgramming/JavaHomework1/src/homework1/Output.java
@@ -3,18 +3,21 @@ package homework1;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 
 public class Output {
     public static void main(String[] args) throws IOException {
         
         InputStream is = System.in;
-        OutputStream os = System.out;
+
         
         int readByteNo;
         byte[] readBytes = new byte[10];
-        while((readByteNo=is.read(readBytes)) !=-1){
-            os.write(readBytes, 0, readByteNo);
+        while(true){
+			readByteNo=is.read(readBytes);
+			if(readByteNo==-1)
+				break;
+			
+			System.out.print(new String(readBytes, 0, readByteNo));
         }
     }
 }
--- WebApplication/src/java/com/mycompany/myapp/controller/Exam07JspController.java
+++ WebApplication/src/java/com/mycompany/myapp/controller/Exam07JspController.java
@@ -1,6 +1,13 @@
 package com.mycompany.myapp.controller;
 
+import com.mycompany.myapp.dto.Board;
+import com.mycompany.myapp.dto.Member;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,15 +18,56 @@ public class Exam07JspController {
 	public String exam01() {
 		return "jsp/exam01";
 	}
-	
+
 	@RequestMapping("/exam02")
 	public String exam02() {
 		return "jsp/exam02";
 	}
-	
+
 	@RequestMapping("/exam03")
 	public String exam03() {
 		return "jsp/exam03";
 	}
 
+//	@RequestMapping("/exam04")
+//	public String exam04(Model model) {
+//		model.addAttribute("name2","홍길동");
+//		model.addAttribute("member2",new Member("홍길동",30));
+//		return "jsp/exam04";
+//	}
+	@RequestMapping("/exam04")
+	public String exam04(HttpServletRequest request) {
+		request.setAttribute("name2", "홍길동");        //HttpServletRequest면 set~~    // set~~은 '한개'만 추가
+		request.setAttribute("member2", new Member("홍길동", 30));
+		return "jsp/exam04";
+	}
+
+	@RequestMapping("/exam05")
+	public String exam05(Model model) {
+		model.addAttribute("name3", "홍길동");     //model이면 add~~   // add~~이면 '여러개' 추가                     // 위처럼 쓰지 않고 model을 쓰는게 보통임.
+		model.addAttribute("member3",new Member("홍길동",30));
+		
+		Board board =new Board();
+		board.setBno(1);
+		board.setBtitle("오늘은 과제");
+		board.setBcontent("과제할 내용 준비");
+		board.setBwriter("감자바");
+		board.setBdate(new Date());
+		model.addAttribute("board", board);
+		
+		
+		List<Board> list = new ArrayList <Board>();
+		for(int i=1;i<=10;i++){
+		Board b = new Board();
+		b.setBno(i);
+		b.setBtitle("제목"+i);
+		b.setBcontent("내용이다"+i);
+		b.setBwriter("글쓴이"+i);
+		b.setBdate(new Date());
+		list.add(b);
+		}
+		model.addAttribute("list", list);
+		
+		return "jsp/exam05";
+	}
 }
